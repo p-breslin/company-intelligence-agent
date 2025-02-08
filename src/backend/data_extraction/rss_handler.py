a88e85df193a2a238d98163b2679dbe3df04fd3b
Peter <peter.breslin@experienceflow.ai>
2025-02-08 00:45:03 -0800

    Separate file for all RSS handling routines

---
+++ src/backend/data_extraction/rss_handler.py
@@ -0,0 +1,60 @@
+import feedparser
+from dateutil import parser
+from utils.config import config
+from urllib.parse import urlparse
+from utils.helpers import compute_hash, clean_html
+
+
+class RSSHandler:
+    def __init__(self, feeds):
+        """Initialize with the database configuration details."""
+        self.schema = config.get_section("schema")
+        self.feeds = feeds
+        self.incomplete = []  # URLs that need additional scraping
+
+
+    def fetch(self):
+        """Fetch and parse RSS feed data."""
+        articles = []
+        for url in self.feeds:
+            feed = feedparser.parse(url)
+            source = getattr(feed.feed, "link", None)
+
+            # Loop through every feed entry and add data to a dict
+            for entry in feed.entries:
+                # Initialize with None values for all database fields
+                data = {field: None for field in self.schema.keys()}
+
+                # Loop over the desired fields in the feed
+                for field in self.schema.keys():
+                    value = getattr(entry, field, None)
+
+                    if field in {"title", "link", "author", "summary"}:
+                        data[field] = clean_html(value, feed="rss") if value and field == "title" else value
+
+                    elif field == "published":
+                        try:
+                            # Parse and convert RSS date string into Python datetime
+                            data[field] = parser.parse(value) if value else None
+                        except Exception as e:
+                            print(f"Error parsing date '{value}': {e}")
+
+                    elif field == "source":
+                        data[field] = urlparse(source).netloc 
+
+                    elif field == "content":
+                    # Use cleaned full content if there; otherwise mark for scraping
+                        if hasattr(entry, field):
+                            data[field] = clean_html(entry.content[0].value, feed="rss")
+                        else:
+                            self.incomplete.append(data["link"])  # Mark for scraping
+
+                    elif field == "tags":
+                        data[field] = value[0]['term'] if value else None
+
+                    elif field == "hash":
+                        data["hash"] = compute_hash(data["title"], data["source"])
+
+                articles.append(data)
+        print(f"RSS feed data fetched. {len(self.incomplete)} articles need scraping.")
+        return articles
