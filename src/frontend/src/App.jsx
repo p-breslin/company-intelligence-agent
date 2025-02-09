287a28ca53eda51147c488f9b18168e7da4d0e2e
Peter <peter.breslin@experienceflow.ai>
2025-02-08 19:37:45 -0800

    cleaning folder

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind is working! 🎉</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
