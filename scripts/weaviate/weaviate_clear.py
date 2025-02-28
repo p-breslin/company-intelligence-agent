7bc14e09c729c472d6b5dd276f2623601731f603
Peter <peter.breslin@experienceflow.ai>
2025-02-28 11:45:02 -0800

    deletes weaviate collection

---
+++ scripts/weaviate/weaviate_clear.py
@@ -0,0 +1,16 @@
+import weaviate
+from utils.config import config
+
+config = config.get_section("weaviate")
+client = weaviate.connect_to_local(port=config["port"])
+
+# Delete all schemas (which deletes all data)
+try:
+    client.schema.delete_all()
+    print("Weaviate database erased successfully.")
+
+except Exception as e:
+    print(f"Error deleting database: {e}")
+
+finally:
+    client.close()
