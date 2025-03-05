e2025669c19ee5957aa5bdf78d6c5406fc4cdcb2
Peter <peter.breslin@experienceflow.ai>
2025-03-05 12:02:45 -0800

    changing names

--- scripts/database_init.py
+++ scripts/database_init.py
@@ -2,9 +2,9 @@ import subprocess
 
 # Define the scripts to run
 scripts = [
-    "scripts/postgres/postgres_clear.py",
+    "scripts/postgres/postgres_del.py",
     "scripts/postgres/postgres_setup.py",
-    "scripts/weaviate/weaviate_clear.py",
+    "scripts/weaviate/weaviate_del.py",
 ]
 
 # Run each script
--- scripts/postgres/postgres_init.py
+++ scripts/postgres/postgres_init.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Define the scripts to run
-scripts = ["scripts/postgres/postgres_clear.py", "scripts/postgres/postgres_setup.py"]
+scripts = ["scripts/postgres/postgres_del.py", "scripts/postgres/postgres_setup.py"]
 
 # Run each script
 for script in scripts:
