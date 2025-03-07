6c3e1400d89f22e0e131be03f261e649292ba8bc
Peter <peter.breslin@experienceflow.ai>
2025-03-07 00:08:34 -0800

    moving collection creation to setup file

--- src/backend/weaviate_pipeline.py
+++ src/backend/weaviate_pipeline.py
@@ -13,31 +13,6 @@ class GenerateEmbeddings:
         # Connect to self-hosted Weaviate instance
         self.client = weaviate.connect_to_local(port=self.config["port"])
 
-        # Define collection if it doesn't exist
-        self.collection_name = self.config["dbname"]
-        if self.collection_name not in self.client.collections.list_all():
-            self.create_schema()
-
-    def create_schema(self):
-        """Create a Weaviate schema based on config file."""
-
-        # Load schema from config
-        schema = [
-            wvc.config.Property(
-                name=field["name"],
-                data_type=wvc.config.DataType(field["dataType"]),
-                skip_vectorization=field["skip"],
-            )
-            for field in self.config["schema"]
-        ]
-
-        self.client.collections.create(
-            name=self.collection_name,
-            vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_contextionary(),
-            properties=schema,
-        )
-        logging.info(f"Created schema for {self.collection_name}")
-
     def check_postgres(self):
         """
         Check for new articles from PostgreSQL and add them to Weaviate.
