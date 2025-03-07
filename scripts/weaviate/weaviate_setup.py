c73496c5fad1c4604837a7992df0b0cba936c553
Peter <peter.breslin@experienceflow.ai>
2025-03-07 00:08:54 -0800

    added collection creation to setup file

---
+++ scripts/weaviate/weaviate_setup.py
@@ -0,0 +1,38 @@
+import logging
+import weaviate
+import weaviate.classes as wvc
+from utils.config import config
+
+config = config.get_section("weaviate")
+client = weaviate.connect_to_local(port=config["port"])
+
+try:
+    # Delete all schemas (which deletes all data)
+    client.collections.delete_all()
+    print("Weaviate database erased successfully.")
+
+    try:
+        # Create new Weaviate database
+        schema = [
+            wvc.config.Property(
+                name=field["name"],
+                data_type=wvc.config.DataType(field["dataType"]),
+                skip_vectorization=field["skip"],
+            )
+            for field in config["schema"]
+        ]
+
+        client.collections.create(
+            name=config["dbname"],
+            vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_contextionary(),
+            properties=schema,
+        )
+        print(f"Created schema for {config['dbname']}")
+
+    except Exception as e:
+        logging.error(f"Error creating database: {e}")
+except Exception as e:
+    logging.error(f"Error deleting database: {e}")
+
+finally:
+    client.close()
